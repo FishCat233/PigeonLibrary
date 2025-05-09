7bb38b9275ae83fbbcd6bc42d1c17d42ae7c278d
FishCat233 <50800434+FishCat233@users.noreply.github.com>
2025-05-09 19:31:51 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -58,7 +58,7 @@ const config: QuartzConfig = {
     transformers: [
       Plugin.FrontMatter(),
       Plugin.CreatedModifiedDate({
-        priority: ["git", "frontmatter"],
+        priority: ["frontmatter", "git"],
       }),
       Plugin.SyntaxHighlighting({
         theme: {
