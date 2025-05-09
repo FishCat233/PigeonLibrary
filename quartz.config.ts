7f739301b90504fc15ecce33604c5c667e23ae20
FishCat233 <50800434+FishCat233@users.noreply.github.com>
2025-05-09 19:07:48 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -18,7 +18,7 @@ const config: QuartzConfig = {
     locale: "zh-CN",
     baseUrl: "library.whispery.top",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "created",
+    defaultDateType: "modified",
     generateSocialImages: false,
     theme: {
       fontOrigin: "googleFonts",
