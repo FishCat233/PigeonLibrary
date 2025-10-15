cd8373ba8dc929af818946bceb4e9f8cf14e2507
FishCat233 <50800434+FishCat233@users.noreply.github.com>
2025-10-15 11:21:31 +0800

    🐛 Fix frontmatter modified date handling

--- quartz/plugins/transformers/frontmatter.ts
+++ quartz/plugins/transformers/frontmatter.ts
@@ -103,7 +103,6 @@ export const FrontMatter: QuartzTransformerPlugin<Partial<Options>> = (userOpts)
             const created = coalesceAliases(data, ["created", "date"])
             if (created) {
               data.created = created
-              data.modified ||= created // if modified is not set, use created
             }
 
             const modified = coalesceAliases(data, [
@@ -113,6 +112,8 @@ export const FrontMatter: QuartzTransformerPlugin<Partial<Options>> = (userOpts)
               "last-modified",
             ])
             if (modified) data.modified = modified
+            data.modified ||= created // if modified is not set, use created
+
             const published = coalesceAliases(data, ["published", "publishDate", "date"])
             if (published) data.published = published
 
@@ -137,20 +138,20 @@ declare module "vfile" {
     frontmatter: { [key: string]: unknown } & {
       title: string
     } & Partial<{
-        tags: string[]
-        aliases: string[]
-        modified: string
-        created: string
-        published: string
-        description: string
-        socialDescription: string
-        publish: boolean | string
-        draft: boolean | string
-        lang: string
-        enableToc: string
-        cssclasses: string[]
-        socialImage: string
-        comments: boolean | string
-      }>
+      tags: string[]
+      aliases: string[]
+      modified: string
+      created: string
+      published: string
+      description: string
+      socialDescription: string
+      publish: boolean | string
+      draft: boolean | string
+      lang: string
+      enableToc: string
+      cssclasses: string[]
+      socialImage: string
+      comments: boolean | string
+    }>
   }
 }
