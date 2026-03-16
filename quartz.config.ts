7ecad3ab0ed1bcbaf9e96c5341b63aec79ffc674
FishCat233 <50800434+FishCat233@users.noreply.github.com>
2026-03-16 10:02:45 +0800

    Update baseUrl configuration

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "zh-CN",
-    baseUrl: "library.whispery.top",
+    baseUrl: "fishcat233.github.io/FishCat233Library/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
